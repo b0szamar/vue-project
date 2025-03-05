cf0ff67906ef56a66c9ded34bca8114b2b4ed212
SzaboMarceII <b0szamar@vasvari.org>
2025-03-05 14:30:14 +0100

    Elkezdve

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+    <div class="container text-center">
+        <h1 class="mt-5 mb-4">Üdvözöllek a Turbo Fodrásznál!</h1>
+        <button @click="goToAppointments" class="btn btn-primary btn-lg">Foglalj időpontot</button>
+    </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css';
+export default {
+    methods: {
+        goToAppointments() {
+            this.$router.push('/idopont');
+        }
+    }
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
---
+++ src/views/IdoPointView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>Időpont foglalás</h1>
+        <div>
+            <label for="time">Szabad időpontok:</label>
+            <select id="time" v-model="selectedTime">
+                <option v-for="time in availableTimes" :value="time">{{ time }}</option>
+            </select>
+        </div>
+        <button @click="bookAppointment">Book Appointment</button>
+    </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css';
+export default {
+    data() {
+        return {
+            selectedDate: '',
+            selectedTime: '',
+            availableTimes: ['10:00 ', '11:00 ', '12:00 ', '13:00 ', '14:00 ', '15:00 ']
+        };
+    },
+    methods: {
+        bookAppointment() {
+
+            console.log('Appointment booked:', this.selectedDate, this.selectedTime);
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
